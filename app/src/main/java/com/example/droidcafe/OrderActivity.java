9fced43347ac856089c857f2942f1cededf7054a
MrLidums <arturs.lidums@gmail.com>
2019-10-24 16:02:22 +0300

    refactored Radio button click method

--- app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -23,26 +23,18 @@ public class OrderActivity extends AppCompatActivity {
     }
 
     public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
         switch (view.getId()) {
             case R.id.sameday:
-                if (checked) {
-                    // Same day service
-                    displayToast(getString(R.string.same_day_messenger_service));
-                }
+                // Same day service
+                displayToast(getString(R.string.same_day_messenger_service));
                 break;
             case R.id.nextday:
-                if (checked) {
-                    // Next day delivery
-                    displayToast(getString(R.string.next_day_ground_delivery));
-                }
+                // Next day delivery
+                displayToast(getString(R.string.next_day_ground_delivery));
                 break;
             case R.id.pickup:
-                if (checked) {
-                    // Pick up
-                    displayToast(getString(R.string.pick_up));
-                }
+                // Pick up
+                displayToast(getString(R.string.pick_up));
                 break;
             default:
                 // Do nothing
